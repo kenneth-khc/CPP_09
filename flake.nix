45cb8dd82454a0a3411f7ac1abb07eda079bb008
Kenneth Cheong <kennethkhc29@gmail.com>
2025-10-15 18:11:45 +0800

    build: add flakes

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "C++ devshell";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells = with pkgs; {
+          default = mkShell {
+            packages = with pkgs; [
+              clang-tools
+              gnumake
+              gdb
+              bear
+            ];
+          };
+          shellHook = ''
+            export SHELL=${lib.getExe pkgs.bash};
+          '';
+        };
+      }
+    );
+}
