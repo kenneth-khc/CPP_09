ebf6b8e22ae33aeb365e280e82a8509da5a7438e
Kenneth Cheong <kennethkhc29@gmail.com>
2025-10-18 16:48:03 +0800

    feat: more robust error checking, better algorithms and more explicit messages
    1) Each field of each record is now parsed to ensure it is valid and
       fits within the constraints of the subject
    2) Trailing and leading whitespaces of every field are trimmed for
       better user experience
    3) Reorganize the code into its own modules

--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -6,13 +6,25 @@
 /*   By: kecheong <kecheong@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/12 20:57:21 by kecheong          #+#    #+#             */
-/*   Updated: 2025/01/12 20:59:44 by kecheong         ###   ########.fr       */
+/*   Updated: 2025/10/18 17:11:28 by kecheong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BITCOINEXCHANGE_HPP
 #define BITCOINEXCHANGE_HPP
 
+#include <string>
+#include <map>
 
+typedef std::map<std::string, float>	Database;
+
+/**
+ Parses a CSV file of Bitcoin exchange rates on a given date into a database. */
+Database	parseCsvFile(const char* = "data.csv");
+
+/**
+ Evaluates a text file with records of Bitcoin amounts on a given date, looking
+ up the exchange rate on the given date and calculating the total value */
+void		evaluate(const Database&, const char* = "input.txt");
 
 #endif
